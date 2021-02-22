106ba0abf31b4be7e6f8df6daa097dab28d80b15
Jorge Oleques <joleques@gmail.com>
2021-02-22 09:24:42 -0300

    Criando produtor em node

---
+++ node/vendas-producer-node/src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import redis from 'redis';
+import { AddressInfo } from "net";
+
+const redisClient = redis.createClient();
+
+const STREAMS_KEY = "eventos-produtos";
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.post(["/node/enviar-produtos"], (_: express.Request, res: express.Response): void => {    
+    redisClient.xadd(
+        STREAMS_KEY,
+        '*', 
+        'produtor_vendas_node', '{"id":3,"descricao":"TV 32","valor":"R$ 3,245.00","caracteristica":{"ano":2020,"marca":"Sony"}', 
+        function (err, id) {
+            if (err) {
+                return console.error(err);
+            }
+            return console.log(id);
+        }
+    );
+    res.send("ok....")
+});
+
+const server = app.listen(port, (): void => {
+    console.log(`Ready on http://localhost:${(server.address() as AddressInfo).port}`);
+});
